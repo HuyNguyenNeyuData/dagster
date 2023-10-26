e9534611a96dd58a9341482d38660ffe113d3e32
Owen Kephart <owen@elementl.com>
2023-10-26 16:51:46 -0700

    add support for true daemon testing

--- python_modules/dagster/dagster_tests/definitions_tests/auto_materialize_tests/test_scenarios.py
+++ python_modules/dagster/dagster_tests/definitions_tests/auto_materialize_tests/test_scenarios.py
@@ -24,6 +24,20 @@ def daemon_instance():
         yield the_instance
 
 
+@pytest.fixture
+def daemon_instance():
+    with instance_for_test(
+        overrides={
+            "run_launcher": {
+                "module": "dagster._core.launcher.sync_in_memory_run_launcher",
+                "class": "SyncInMemoryRunLauncher",
+            }
+        }
+    ) as the_instance:
+        set_auto_materialize_paused(the_instance, False)
+        yield the_instance
+
+
 @pytest.mark.parametrize("scenario", all_scenarios, ids=[scenario.id for scenario in all_scenarios])
 def test_scenario_fast(scenario: AssetDaemonScenario) -> None:
     scenario.evaluate_fast()
